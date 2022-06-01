b20b72cbc45ef2872b9758ac48784e7573d00501
AIDDEI <aidendeightonschool@gmail.com>
2022-06-01 15:41:53 +0200

    Horizontal collision (alleen de linkercollision werkt nog niet)

--- src/ts/movement_test.ts
+++ src/ts/movement_test.ts
@@ -2,9 +2,11 @@ import * as PIXI from 'pixi.js';
 import testChar from '../images/Char1_1.png';
 import testBack from '../images/test_background2.jpg';
 import testGround from '../images/test_ground2.jpg';
+import testBlock from '../images/block.jpg';
 
 import { Char } from './test_char';
 import { Ground } from './test_ground';
+import { Block } from './test_block';
 
 export class Game{
     pixiWidth = 800;
@@ -15,6 +17,7 @@ export class Game{
 
     char : Char;
     ground : Ground;
+    block : Block;
 
     constructor(){
         this.pixi = new PIXI.Application({width: this.pixiWidth, height: this.pixiHeight});
@@ -26,7 +29,8 @@ export class Game{
         this.loader
             .add('charTexture', testChar)
             .add('backgroundTexture', testBack)
-            .add('groundTexture', testGround);
+            .add('groundTexture', testGround)
+            .add('blockTexture', testBlock);
         this.loader.load(()=>this.loadCompleted());
     }
 
@@ -36,8 +40,11 @@ export class Game{
         background.width = this.pixiWidth;
         this.pixi.stage.addChild(background);
 
-        this.ground = new Ground(this.loader.resources["groundTexture"].texture!, this);
+        this.ground = new Ground(this.loader.resources["groundTexture"].texture!);
         this.pixi.stage.addChild(this.ground);
+
+        this.block = new Block(this.loader.resources["blockTexture"].texture!);
+        this.pixi.stage.addChild(this.block);
 
         this.char = new Char(this.loader.resources["charTexture"].texture!);
         this.pixi.stage.addChild(this.char);
@@ -48,10 +55,17 @@ export class Game{
     update(delta: number){
         this.char.update(delta);
 
-        if(this.char.collisionVertical(this.ground) && this.char.y + this.char.height < this.ground.y + this.char.yspeed){
+        if(this.char.collisionVerticalTop(this.ground) && this.char.y + this.char.height < this.ground.y + this.char.yspeed){
             this.char.y = this.ground.y - this.char.height;
             this.char.yspeed = 0;
         }
+
+        if(this.char.collisionVerticalTop(this.block) && this.char.y + this.char.height < this.block.y + this.char.yspeed){
+            this.char.y = this.block.y - this.char.height;
+            this.char.yspeed = 0;
+        }
+
+        this.char.collisionHorizontal(this.block);
     }
 }
 
