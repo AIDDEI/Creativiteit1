8b4b249649683e733403e5a3ab07449977b4de72
BoyK99 <blgkik@hotmail.com>
2022-06-08 12:07:41 +0200

    UI + movement_boy add

---
+++ src/ts/UI.ts
@@ -0,0 +1,33 @@
+import * as PIXI from 'pixi.js';
+
+export class UI extends PIXI.Container {
+
+    scoreField:PIXI.Text
+
+    doomClock:number = 3600 
+
+    constructor(){
+        super();
+        const style = new PIXI.TextStyle({
+            fontFamily: 'ArcadeFont',
+            fontSize: 40,
+            fontWeight: 'bold',
+            fill: ['#ffffff']
+        })
+    
+        this.scoreField = new PIXI.Text(`Timer: `, style)
+        this.addChild(this.scoreField)
+        this.scoreField.x = 10
+        this.scoreField.y = 10
+    }
+
+    public update(delta:number){
+        this.doomClock-=delta
+        let secondsLeft = Math.floor(this.doomClock / 60)
+        if(this.doomClock <= 0) {
+            console.log("Doomsday has come!")
+        } else {
+            console.log(`Only ${secondsLeft} seconds left!`)
+        }
+    }
+}
---
+++ src/ts/movement_test_boy.ts
@@ -0,0 +1,89 @@
+import * as PIXI from 'pixi.js';
+import testChar from '../images/Char1_1.png';
+import testBack from '../images/test_background2.jpg';
+import testGround from '../images/test_ground2.jpg';
+import testBlock from '../images/block.jpg';
+
+import { Char } from './test_char';
+import { Ground } from './test_ground';
+import { Block } from './test_block';
+import { UI } from './UI';
+
+export class Game{
+    pixiWidth = 800;
+    pixiHeight = 450;
+
+    pixi : PIXI.Application;
+    loader : PIXI.Loader;
+
+    char : Char;
+    ground : Ground;
+    block : Block;
+
+    interface : UI;
+
+    constructor(){
+        this.pixi = new PIXI.Application({width: this.pixiWidth, height: this.pixiHeight});
+        this.pixi.stage.interactive = true;
+        this.pixi.stage.hitArea = this.pixi.renderer.screen;
+        document.body.appendChild(this.pixi.view);
+
+        this.loader = new PIXI.Loader();
+        this.loader
+            .add('charTexture', testChar)
+            .add('backgroundTexture', testBack)
+            .add('groundTexture', testGround)
+            .add('blockTexture', testBlock);
+        this.loader.load(()=>this.loadCompleted());
+    }
+
+    loadCompleted(){
+        let background = new PIXI.Sprite(this.loader.resources["backgroundTexture"].texture!);
+        background.height = this.pixiHeight;
+        background.width = this.pixiWidth;
+        this.pixi.stage.addChild(background);
+
+        this.ground = new Ground(this.loader.resources["groundTexture"].texture!);
+        this.pixi.stage.addChild(this.ground);
+
+        this.block = new Block(this.loader.resources["blockTexture"].texture!);
+        this.pixi.stage.addChild(this.block);
+
+        this.char = new Char(this.loader.resources["charTexture"].texture!);
+        this.pixi.stage.addChild(this.char);
+
+        this.interface = new UI()
+        this.pixi.stage.addChild(this.interface)
+
+        this.pixi.ticker.add((delta) => this.update(delta));
+    }
+
+    doomClock:number = 600 
+
+    private update(delta: number){
+        this.char.update(delta);
+
+        if(this.char.collisionVerticalTop(this.ground) && this.char.y + this.char.height < this.ground.y + this.char.yspeed){
+            this.char.y = this.ground.y - this.char.height;
+            this.char.yspeed = 0;
+        }
+
+        if(this.char.collisionVerticalTop(this.block) && this.char.y + this.char.height < this.block.y + this.char.yspeed){
+            this.char.y = this.block.y - this.char.height;
+            this.char.yspeed = 0;
+        }
+
+        this.char.collisionHorizontal(this.ground);
+        this.char.collisionHorizontal(this.block);
+        
+        this.doomClock-=delta
+        let secondsLeft = Math.floor(this.doomClock / 60)
+        if(this.doomClock <= 0) {
+            console.log("Doomsday has come!")
+        } else {
+            console.log(`Only ${secondsLeft} seconds left!`)
+        }
+    }
+}
+
+new Game();
