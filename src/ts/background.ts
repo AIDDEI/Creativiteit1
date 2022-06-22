0b05bfd56c567c9fc450cbf037546327a5845801
BoyK99 <blgkik@hotmail.com>
2022-06-22 12:48:48 +0200

    backround

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -1,12 +1,19 @@
 // Import PIXI
 import * as PIXI from 'pixi.js';
 
+// Import class
+import { Char } from './test_char';
+
 export class Background extends PIXI.TilingSprite{
+
+    char : Char;
+
     constructor(texture: PIXI.Texture, width: number, height: number){
         super(texture, width, height);
     }
 
     public update() {
+        //  if (char.walkRight) { }
         this.tilePosition.x -= 0.4 
     }
 }
